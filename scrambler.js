295f1a9ca6cbac8fee04f3bb95fbdee421b5826e
MJE10 <michaeljelia@gmail.com>
2022-03-30 16:09:34 -0400

    Added environment variable paths

--- scrambler.js
+++ scrambler.js
@@ -9,12 +9,15 @@ module.exports = class Scrambler {
         if (type === "apple") type = "three";
         console.log('scramble generated!');
 
-        const scramble = execSync("C:\\Users\\micha\\OneDrive\\Desktop\\Programs\\tnoodle-cli-1.0.0-win_x64\\tnoodle-cli-win_x64\\bin\\tnoodle scramble -p "+type).toString().trim();
+        const tnoodlePath = process.env.tnoodlePath;
+        const imagesPath = process.env.imagesPath;
+
+        const scramble = execSync(tnoodlePath + " scramble -p "+type).toString().trim();
         console.log(scramble);
 
         // generate image
 
-        execSync('C:\\Users\\micha\\OneDrive\\Desktop\\Programs\\tnoodle-cli-1.0.0-win_x64\\tnoodle-cli-win_x64\\bin\\tnoodle draw -o "C:\\Users\\micha\\WebstormProjects\\webcompbot\\public\\images\\' + scramble + '.svg" -p ' + type + ' -s "'+ scramble);
+        execSync(tnoodlePath + ' draw -o "' + imagesPath + scramble + '.svg" -p ' + type + ' -s "'+ scramble);
         console.log('image generated!');
 
         return scramble;
