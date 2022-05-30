65fd2999dbb9ff4807e7fcf2260214de397d3fc5
MJE10 <63426573+MJE10@users.noreply.github.com>
2022-05-30 14:22:36 -0400

    make sure directory.json exists

--- js/envChecker.js
+++ js/envChecker.js
@@ -8,6 +8,16 @@ module.exports = class EnvChecker {
         if (!fs.existsSync(".env")) {
             throw new Error("No .env file found!");
         }
+        // check that competitions folder exists
+        if (!fs.existsSync("competitions")) {
+            // make it
+            fs.mkdirSync("competitions");
+        }
+        // check that competitions/directory.json exists
+        if (!fs.existsSync("competitions/directory.json")) {
+            // make it
+            fs.writeFileSync("competitions/directory.json", "{}");
+        }
         // check that all required variables are set
         const requiredVariables = [
             "token",
