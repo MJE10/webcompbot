f2c8d5310acd5dd68ba850cd8e374cf991dd8dd5
Michael Elia <michaeljelia@gmail.com>
2022-07-20 19:39:22 -0400

    Add file documentation for index.ts

--- index.ts
+++ index.ts
@@ -1,6 +1,19 @@
 "use strict";
 
-// check that all environment variables are set
+/**
+ * file: index.ts
+ * author: Michael Elia, michaeljelia@gmail.com
+ * date: 2022/07/20
+ * ---
+ * This is the root file for the server. It uses three objects to manage competitions:
+ * - A Competition object to represent all data and methods relating to competition
+ * - A DiscordClient object to communicate with the Discord bot for authentication
+ * - A custom WebServer object to handle communications between web clients and the Competition
+ *
+ * index.ts creates the three objects then links appropriate methods for intercommunication. See internal
+ * comments for details on the purpose and implementation for each object.
+ */
+
 import checkEnv from "./js/envChecker";
 checkEnv();
 
